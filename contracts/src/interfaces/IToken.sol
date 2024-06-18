85789af4a9504aacbc127cc4950680b8aed2cee3
Kacper Bak <kacper@quantstamp.com>
2024-06-18 13:17:13 +0400

    separated examples from the scripts

--- contracts/src/interfaces/IToken.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.8.24;
-interface IToken {
-    function transfer(address to, uint256 amount) external;
-    function balanceOf(address account) external view returns (uint256);
-    function changeOwner(address _newOwner) external;
-}
--- contracts/src/interfaces/constants/Constants.sol
+++
@@ -1,3 +0,0 @@
-pragma solidity 0.8.24;
-uint256 constant MAX_UINT256 = type(uint256).max;
-address constant ZERO_ADDRESS = address(0);
